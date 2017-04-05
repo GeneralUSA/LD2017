471dfb99921e0192e3d1023c8175a68b7f23235e
GeneralUSA <email @ github.com>
2017-04-04 22:19:54 -0400

    Commit staged--tutorial -- https://www.youtube.com/watch?v=nRI3OVavUbE

---
+++ app/scripts/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kimberlyApp
+ * @description
+ * # kimberlyApp
+ *
+ * Main module of the application.
+ *
+ * Hi Kimberly,
+ *
+ * If you want to add pages here and javascript code, check out the $routeProvider below.
+ */
+
+angular
+  .module('kimberlyApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+      })
+      .when('/calendar', {
+        templateUrl: 'views/calendar.html',
+        controller: 'CalendarCtrl',
+      })
+	  .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+      })
+      .when('/legalrss', {
+        templateUrl: 'views/resources.html',
+        controller: 'LegalRssCtrl',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/legalrss.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kimberlyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ *
+ * Hi Kimberly, this is the Javascript code for the home page, that corresponds to app/views/main.html
+ *
+ * When you have time, research MVC (Model View Controller) coding, and Object Oriented Programming
+ *
+ * The .controller('MainCtrl'), is the name of the controller. If you take a look at /app/scripts/app.js, you will see a table of contents, which
+ * uses this javascript file, and app/views/main.html.
+ *
+ * 
+ */
+
+angular.module('kimberlyApp')
+  .controller('LegalRssCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
